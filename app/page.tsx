387604146ba96eb9360a127af277505a0ca96aa1
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2024-08-21 23:46:48 +0530

    Added Authentication flow

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ import { useCallback } from 'react';
 import toast from 'react-hot-toast';
 import { graphqlClient } from '@/client/api';
 import { verifyUserGoogleTokenQuery } from '@/graphql/query/user';
+import { useCurrentUser } from '@/hooks/user';
+import { useQueryClient } from '@tanstack/react-query';
+import Image from 'next/image';
+// import { useCurrentUser } from '@/hooks/user';
 
 interface TwitterSidebarButton {
   title: string;
@@ -51,9 +55,14 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 
 export default function Home() {
 
+  const {user} = useCurrentUser()
+  const queryClient = useQueryClient();
+
   const handleLoginWithGoogle = useCallback(async (cred: CredentialResponse) => {
     const googleToken = cred.credential
     if(!googleToken) return toast.error('Google token not found')
+    console.log(googleToken)
+  
     const { verifyGoogleToken } = await graphqlClient.request(
       verifyUserGoogleTokenQuery, {token: googleToken}
     );
@@ -62,12 +71,16 @@ export default function Home() {
     console.log(verifyGoogleToken)
 
     if(verifyGoogleToken) window.localStorage.setItem('twitter_token', verifyGoogleToken)
-  }, [])
+    
+    await queryClient.invalidateQueries({
+      queryKey: ["current-user"]
+    });
+  }, [queryClient])
 
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-4 pt-1 ml-28">
+        <div className="col-span-4 pt-1 ml-28 relative">
           <div className="text-2xl cursor-pointer w-fit p-4 transition-all h-fit hover:bg-gray-600 rounded-full">
             <BsTwitter />
           </div>
@@ -86,17 +99,34 @@ export default function Home() {
               </button>
             </div>
           </div>
+        {user && (
+        <div className='absolute bottom-5 flex gap-2 items-center bg-slate-800
+        px-3 py-2 rounded-full'>
+          {user && user.profileImageURL && (
+          <Image 
+            className='rounded-full'
+            src={user?.profileImageURL} 
+            alt='user-image' 
+            height={50} 
+            width={50} 
+          />
+          )}
+          <div>
+            <h3 className='text-xl'>{user.firstName}</h3>
+            <h3 className='text-xl'>{user.lastName}</h3>
+          </div>
+        </div>)}
         </div>
         <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-y-scroll border-gray-600">
           <FeedCard />
         </div>
         <div className="col-span-3 p-5">
-          <div className='p-5 bg-slate-700 rounded-lg'>
+          {!user && <div className='p-5 bg-slate-700 rounded-lg'>
             <h1 className='my-2 text-2xl'>New to Twitter</h1>
               <GoogleLogin
                   onSuccess={handleLoginWithGoogle}
               />
-          </div>
+          </div>}
         </div>
       </div>
     </div>
